f619729d88b76a6b21d86bc00784d9a80b23c8d8
mark <mr.karimi1377@gmail.com>
2021-08-22 22:04:03 +0430

    settings page created

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:igsaver/constants.dart';
+import 'package:igsaver/widgets/settings_card.dart';
 
 class Settings extends StatefulWidget {
   static const route = '/settings';
@@ -8,8 +10,38 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  bool clipboardSwitch = false;
+  bool backgroundSwitch = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        elevation: 0,
+        title: Text('Settings'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SettingsCard(
+              title: 'Watch Clipboard',
+              description:
+                  'Automatically start downloading when URL copied to the clipboard.',
+              switchValue: clipboardSwitch,
+              switchCallback: (bool value) {},
+            ),
+            SettingsCard(
+              title: 'Run In Background',
+              description:
+                  'Allow this app to run a service in background to watch clipboard and download posts.',
+              switchValue: clipboardSwitch,
+              switchCallback: (bool value) {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
