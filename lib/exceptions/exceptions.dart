b2af81379f9de4f3eccc7841bef58c71abfd476f
mark <mr.karimi1377@gmail.com>
2021-08-24 21:50:20 +0430

    exceptions moved to a seperate file

---
+++ lib/exceptions/exceptions.dart
@@ -0,0 +1,17 @@
+class NotFoundException implements Exception {
+  final String message;
+
+  NotFoundException(this.message);
+
+  @override
+  String toString() => message;
+}
+
+class UnknownPostTypeException implements Exception {
+  final String message;
+
+  UnknownPostTypeException(this.message);
+
+  @override
+  String toString() => message;
+}
