02fe8d08576f2ee99a4a7fe38a02276ed61a0845
mark <mr.karimi1377@gmail.com>
2021-08-28 12:59:03 +0430

    settings service added

---
+++ lib/services/settings_service.dart
@@ -0,0 +1,23 @@
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class SettingsService {
+  static Future<void> initialize() async {
+    await Hive.initFlutter();
+    await Hive.openBox('settings');
+  }
+
+  static const clipboard = 'CLIPBOARD';
+  static const vibrate = 'VIBRATE';
+  static const imagesOnlySwitch = 'IMAGES_ONLY';
+
+  bool get(String key, bool defaultValue) {
+    var settings = Hive.box('settings');
+    return settings.get(key, defaultValue: defaultValue);
+  }
+
+  void set(String key, bool value) {
+    var settings = Hive.box('settings');
+    settings.put(key, value);
+  }
+}
