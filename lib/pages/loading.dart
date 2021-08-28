ef206054aa095575b1fa91a051a940e0adebfa3c
mark <mr.karimi1377@gmail.com>
2021-08-28 20:39:22 +0430

    some refactore

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,7 +13,7 @@ class Loading extends StatelessWidget {
     try {
       final result = await InternetAddress.lookup('google.com');
       return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-    } on SocketException catch (_) {
+    } on SocketException {
       return false;
     }
   }
