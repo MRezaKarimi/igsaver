e8ccef687ac25c33f5456a1b90c244db88d2d57a
mark <mr.karimi1377@gmail.com>
2021-09-05 11:10:40 +0430

    download history and item added

---
+++ lib/models/download_history.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+enum Status {
+  inProgress,
+  successful,
+  failed,
+}
+
+class DownloadHistory extends ChangeNotifier {
+  List<DownloadItem> _downloadsList = [];
+  int _index = -1;
+
+  List<DownloadItem> get downloadsList => _downloadsList;
+
+  int addItem(String title, int total) {
+    _index++;
+    _downloadsList.add(DownloadItem(title, total));
+    notifyListeners();
+    return _index;
+  }
+
+  void setCount(int newCount, int index) {
+    if (index < _downloadsList.length) {
+      _downloadsList.elementAt(index).count = newCount;
+      notifyListeners();
+    }
+  }
+}
+
+class DownloadItem extends ChangeNotifier {
+  final String title;
+  final int total;
+  int _count = 0;
+  Status status = Status.inProgress;
+
+  DownloadItem(this.title, this.total);
+
+  int get count => _count;
+
+  set count(int newCount) {
+    _count = newCount;
+    if (_count == total) {
+      status = Status.successful;
+    }
+  }
+}
