50921cb3c541937dab08b3eba2034ce2e9b0bf85
mark <mr.karimi1377@gmail.com>
2021-09-05 11:03:22 +0430

    wrap materialapp with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:igsaver/pages/history.dart';
 import 'package:igsaver/pages/loading.dart';
 import 'package:igsaver/pages/profile_download.dart';
 import 'package:igsaver/pages/settings.dart';
 import 'package:igsaver/pages/home.dart';
+import 'package:igsaver/models/download_history.dart';
 
 void main() {
   runApp(IGSaver());
@@ -12,16 +14,19 @@ void main() {
 class IGSaver extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'IGSaver',
-      initialRoute: Loading.route,
-      routes: {
-        Loading.route: (context) => Loading(),
-        Home.route: (context) => Home(),
-        Settings.route: (context) => Settings(),
-        History.route: (context) => History(),
-        ProfileDownload.route: (context) => ProfileDownload(),
-      },
+    return ChangeNotifierProvider<DownloadHistory>(
+      create: (_) => DownloadHistory(),
+      child: MaterialApp(
+        title: 'IGSaver',
+        initialRoute: Loading.route,
+        routes: {
+          Loading.route: (context) => Loading(),
+          Home.route: (context) => Home(),
+          Settings.route: (context) => Settings(),
+          History.route: (context) => History(),
+          ProfileDownload.route: (context) => ProfileDownload(),
+        },
+      ),
     );
   }
 }
