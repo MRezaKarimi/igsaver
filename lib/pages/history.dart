c50ee5146609ab5968880fb3c4c617ecfcf5cdab
mark <mr.karimi1377@gmail.com>
2021-09-09 12:10:38 +0430

    history page completed

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:igsaver/constants.dart';
+import 'package:igsaver/models/download_history.dart';
+import 'package:igsaver/pages/image_viewer.dart';
+import 'package:provider/provider.dart';
 
 class History extends StatefulWidget {
   static const route = '/history';
@@ -11,18 +14,15 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
-  Future<Iterable> _getImages() async {
-    final dir = Directory('/storage/emulated/0/IGSaver/images');
-    final List<FileSystemEntity> entities = await dir.list().toList();
-    final Iterable<File> files = entities.whereType<File>();
-    return files;
+  Future<List<File>> _getImages() async {
+    Directory dir = Directory('/storage/emulated/0/IGSaver/images');
+    List<FileSystemEntity> entities = await dir.list().toList();
+    Iterable<File> files = entities.whereType<File>();
+    return files.toList();
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map> myProducts = List.generate(
-        100000, (index) => {"id": index, "name": "Product $index"}).toList();
-    _getImages();
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -31,57 +31,91 @@ class _HistoryState extends State<History> {
         title: Text('History'),
       ),
       body: SafeArea(
-        child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              mainAxisSpacing: 5,
-              crossAxisSpacing: 5,
-            ),
-            /*SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-            ),*/
-            itemCount: myProducts.length,
-            itemBuilder: (BuildContext ctx, index) {
-              return Container(
-                alignment: Alignment.center,
-                child: Text(myProducts[index]["name"]),
-                color: Colors.amber,
+        child: FutureBuilder<List<File>>(
+          future: _getImages(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 3,
+                  crossAxisSpacing: 3,
+                ),
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        ImageViewer.route,
+                        arguments: {
+                          'image_path': snapshot.data![index].path,
+                        },
+                      );
+                    },
+                    child: Image.file(
+                      snapshot.data![index],
+                      cacheWidth: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
               );
-            }),
-        /*GridView.count(
-          crossAxisCount: 4,
-          children: [
-            Container(
-              color: Colors.amber,
-              height: 200,
-              width: 200,
-            ),
-            Container(
-              color: Colors.blue,
-              height: 200,
-              width: 200,
-            ),
-            Container(
-              color: Colors.green,
-              height: 200,
-              width: 200,
-            ),
-            Container(
-              color: Colors.red,
-              height: 200,
-              width: 200,
-            ),
-            Container(
-              color: Colors.purple,
-              height: 200,
-              width: 200,
-            ),
-          ],
-        ),*/
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'An Unexpected Error Occurred ',
+                  style: TextStyle(fontSize: 16),
+                ),
+              );
+            }
+            return Center(
+              child: Text(
+                'Loading Images',
+                style: TextStyle(fontSize: 16),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
 }
+
+// Consumer<DownloadHistory>(
+//           builder: (context, downloadHistory, child) {
+//             List<Widget> listViewItems = [];
+//             for (var item in downloadHistory.downloadsList) {
+//               listViewItems.add(
+//                 ListTile(
+//                   leading: Icon(CupertinoIcons.arrow_down_circle),
+//                   title: Text(item.title),
+//                   subtitle: LinearProgressIndicator(
+//                     value: item.percentage,
+//                     color: kPrimaryColor,
+//                     backgroundColor: Colors.grey[200],
+//                   ),
+//                 ),
+//               );
+//             }
+//             return ListView(
+//               reverse: true,
+//               children: listViewItems,
+//               // ListTile(
+//               //   leading: Icon(CupertinoIcons.arrow_down_circle),
+//               //   title: Text('Natgeo iuho8iugyuv.jpeg'),
+//               //   subtitle: LinearProgressIndicator(
+//               //     value: .25,
+//               //     color: kPrimaryColor,
+//               //     backgroundColor: Colors.grey[200],
+//               //   ),
+//               // ),
+//               // ListTile(
+//               //   leading: Icon(CupertinoIcons.check_mark_circled),
+//               //   title: Text('Natgeo iuho8iugyuv.jpeg'),
+//               //   subtitle: Text('Download Completed'),
+//               // ),
+//             );
+//           },
+//         ),
+
