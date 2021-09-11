6c0a9308fda352bf674c8d39a01646b653f92206
mark <mr.karimi1377@gmail.com>
2021-09-11 10:17:42 +0430

    two type of butt created

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -13,6 +13,34 @@ class RoundedButton extends StatelessWidget {
       child: Text(
         text,
         style: TextStyle(
+          color: kPrimaryColor,
+          fontSize: 18,
+        ),
+      ),
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        side: BorderSide(color: kPrimaryColor, width: 2),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
+
+class FilledRoundedButton extends StatelessWidget {
+  final String text;
+  final void Function() onPressed;
+
+  FilledRoundedButton({required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      child: Text(
+        text,
+        style: TextStyle(
           color: Colors.white,
           fontSize: 18,
         ),
