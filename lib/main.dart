a446f90b2f51fce7d55416bdd82cced9b5e95226
mark <mr.karimi1377@gmail.com>
2021-09-12 22:20:56 +0430

    provider removed. select post page route added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:igsaver/pages/image_viewer.dart';
+import 'package:igsaver/pages/post_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:igsaver/pages/history.dart';
 import 'package:igsaver/pages/loading.dart';
@@ -15,20 +16,18 @@ void main() {
 class IGSaver extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<DownloadHistory>(
-      create: (_) => DownloadHistory(),
-      child: MaterialApp(
-        title: 'IGSaver',
-        initialRoute: Loading.route,
-        routes: {
-          Loading.route: (context) => Loading(),
-          Home.route: (context) => Home(),
-          Settings.route: (context) => Settings(),
-          History.route: (context) => History(),
-          ProfileDownload.route: (context) => ProfileDownload(),
-          ImageViewer.route: (context) => ImageViewer(),
-        },
-      ),
+    return MaterialApp(
+      title: 'IGSaver',
+      initialRoute: Loading.route,
+      routes: {
+        Loading.route: (context) => Loading(),
+        Home.route: (context) => Home(),
+        Settings.route: (context) => Settings(),
+        History.route: (context) => History(),
+        ProfileDownload.route: (context) => ProfileDownload(),
+        ImageViewer.route: (context) => ImageViewer(),
+        SelectPost.route: (context) => SelectPost(),
+      },
     );
   }
 }
