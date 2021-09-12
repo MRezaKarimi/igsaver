d1b3fe7e3c2fee49634a389ff5df78162865ab3c
mark <mr.karimi1377@gmail.com>
2021-09-12 22:31:26 +0430

    add functionality to gridItem

--- lib/widgets/grid_item.dart
+++ lib/widgets/grid_item.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:igsaver/constants.dart';
+import 'package:igsaver/models/posts_list.dart';
 
 class GridItem extends StatefulWidget {
   final data;
-  final bool selected;
-  final void Function() onTap;
+  final int index;
+  PostsList selectedPosts;
 
-  GridItem({required this.data, required this.onTap, required this.selected});
+  GridItem(
+      {required this.data, required this.index, required this.selectedPosts});
 
   @override
   _GridItemState createState() => _GridItemState();
@@ -39,6 +41,11 @@ class _GridItemState extends State<GridItem> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        if (selected) {
+          widget.selectedPosts.remove(widget.index);
+        } else {
+          widget.selectedPosts.add(widget.index, widget.data);
+        }
         setState(() {
           selected = !selected;
         });
@@ -55,63 +62,6 @@ class _GridItemState extends State<GridItem> {
           Align(
             alignment: AlignmentDirectional(0.9, -0.9),
             child: _getTileIcon(widget.data),
-          ),
-          Align(
-            alignment: AlignmentDirectional.bottomStart,
-            child: selected
-                ? Icon(
-                    CupertinoIcons.check_mark_circled_solid,
-                    color: kPrimaryColor,
-                  )
-                : null,
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class GGridItem extends StatelessWidget {
-  final data;
-  final bool selected;
-  final void Function() onTap;
-
-  GGridItem({required this.data, required this.onTap, required this.selected});
-
-  Widget? _getTileIcon(data) {
-    if (data['__typename'] == 'GraphSidecar') {
-      return Icon(
-        CupertinoIcons.rectangle_fill_on_rectangle_fill,
-        color: Colors.white,
-        size: 20,
-      );
-    } else if (data['__typename'] == 'GraphVideo') {
-      return Icon(
-        CupertinoIcons.play_fill,
-        color: Colors.white,
-        size: 20,
-      );
-    } else {
-      return null;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Stack(
-        children: [
-          Align(
-            alignment: AlignmentDirectional.center,
-            child: Image.network(
-              data['thumbnail_resources'][0]['src'],
-              fit: BoxFit.cover,
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional(0.9, -0.9),
-            child: _getTileIcon(data),
           ),
           Align(
             alignment: AlignmentDirectional.bottomStart,
