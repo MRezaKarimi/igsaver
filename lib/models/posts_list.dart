0451b15c30550b948f7af76f00bef77cb91448b2
mark <mr.karimi1377@gmail.com>
2021-09-15 14:06:29 +0430

    add data type for getters

--- lib/models/posts_list.dart
+++ lib/models/posts_list.dart
@@ -4,9 +4,9 @@ import 'package:flutter/widgets.dart';
 class PostsList extends ChangeNotifier {
   Map<int, dynamic> _list = {};
 
-  get list => _list;
+  Map<int, dynamic> get list => _list;
 
-  get length => _list.length;
+  int get length => _list.length;
 
   void add(int index, data) {
     _list[index] = data;
