7508ca0d1af787b68e3f34e776755d695194a044
mark <mr.karimi1377@gmail.com>
2021-09-21 18:49:03 +0430

    hive box not found bug fixed

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -3,24 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:igsaver/constants.dart';
 import 'package:igsaver/pages/home.dart';
 import 'package:igsaver/services/settings_service.dart';
-import 'package:igsaver/widgets/rounded_dialog.dart';
 import 'package:igsaver/widgets/error_dialog.dart';
 
 class Loading extends StatelessWidget {
   static const String route = '/';
 
-  Future<bool> _hasNetwork() async {
-    Future.delayed(Duration(seconds: 1));
+  Future<void> _checkNetworkConnection(BuildContext context) async {
+    bool isConnected;
+    await Future.delayed(Duration(seconds: 1));
+
     try {
       final result = await InternetAddress.lookup('google.com');
-      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+      isConnected = result.isNotEmpty && result[0].rawAddress.isNotEmpty;
     } on SocketException {
-      return false;
+      isConnected = false;
     }
-  }
 
-  void _checkNetworkConnection(BuildContext context) async {
-    bool isConnected = await _hasNetwork();
     if (isConnected) {
       Navigator.pushReplacementNamed(context, Home.route);
     } else {
@@ -33,10 +31,14 @@ class Loading extends StatelessWidget {
     }
   }
 
+  void _initialStuffs(BuildContext context) async {
+    await SettingsService.initialize();
+    await _checkNetworkConnection(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    _checkNetworkConnection(context);
-    SettingsService.initialize();
+    _initialStuffs(context);
     return Scaffold(
       backgroundColor: kPrimaryColor,
       body: Center(
