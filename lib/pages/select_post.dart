921e8b69988e907f4642a29fd8e6809fa8120c43
mark <mr.karimi1377@gmail.com>
2021-09-23 09:37:57 +0330

    some refactor

--- lib/pages/select_post.dart
+++ lib/pages/select_post.dart
@@ -9,76 +9,70 @@ import 'package:igsaver/widgets/grid_item.dart';
 import 'package:provider/provider.dart';
 
 class SelectPost extends StatelessWidget {
-  static const route = '/post_picker';
+  static const route = '/select_post';
 
-  /// Scaffold and it's subtree moved to a separate method because provider not recognizing
-  /// Scaffold as Widget and throws error.
-  Widget _buildPage(BuildContext context, Widget? _) {
+  @override
+  Widget build(BuildContext context) {
     Map args = ModalRoute.of(context)?.settings.arguments as Map;
     int userID = int.parse(args['userID']);
     InstagramProfileDownloader profileDownloader = InstagramProfileDownloader();
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        elevation: 0,
-        title: CounterText(),
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: StreamBuilder<List<dynamic>>(
-                stream: profileDownloader.getProfilePosts(userID),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 5,
-                        mainAxisSpacing: 5,
-                      ),
-                      itemCount: snapshot.data?.length,
-                      itemBuilder: (context, index) {
-                        var data = snapshot.data![index];
-                        return GridItem(
-                          data: data,
-                          index: index,
-                          selectedPosts:
-                              Provider.of<PostsList>(context, listen: false),
-                        );
-                      },
-                    );
-                  } else {
-                    return Center(
-                      child: Text('Loading Data'),
-                    );
-                  }
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: FilledRoundedButton(
-                text: 'Download Selected',
-                onPressed: () {
-                  profileDownloader.downloadSelectedPosts(
-                      Provider.of<PostsList>(context, listen: false));
-                },
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return ChangeNotifierProvider<PostsList>(
       create: (context) => PostsList(),
-      builder: _buildPage,
+      builder: (context, _) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: kPrimaryColor,
+          elevation: 0,
+          title: CounterText(),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: StreamBuilder<List<dynamic>>(
+                  stream: profileDownloader.getProfilePosts(userID),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 5,
+                        ),
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, index) {
+                          var data = snapshot.data![index];
+                          return GridItem(
+                            data: data,
+                            index: index,
+                            selectedPosts:
+                                Provider.of<PostsList>(context, listen: false),
+                          );
+                        },
+                      );
+                    } else {
+                      return Center(
+                        child: Text('Loading Data'),
+                      );
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: FilledRoundedButton(
+                  text: 'Download Selected',
+                  onPressed: () {
+                    profileDownloader.downloadSelectedPosts(
+                        Provider.of<PostsList>(context, listen: false));
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
