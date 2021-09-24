3002d651aac8a5c4e6395039cc5808595acc9668
mark <mr.karimi1377@gmail.com>
2021-09-24 18:42:23 +0330

    check permission added

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,14 +1,21 @@
+import 'dart:html';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:igsaver/constants.dart';
 import 'package:igsaver/pages/home.dart';
 import 'package:igsaver/services/settings_service.dart';
 import 'package:igsaver/widgets/error_dialog.dart';
 
+/// Shows a loading screen to the user and does initial staffs behind the scene.
 class Loading extends StatelessWidget {
   static const String route = '/';
 
-  Future<void> _checkNetworkConnection(BuildContext context) async {
+  /// Checks whether device is connected to the network or not.
+  ///
+  /// If device has network connection, returns true.
+  /// Otherwise shows an error dialog and returns false.
+  Future<bool> _checkNetworkConnection(BuildContext context) async {
     bool isConnected;
     await Future.delayed(Duration(seconds: 1));
 
@@ -20,7 +27,7 @@ class Loading extends StatelessWidget {
     }
 
     if (isConnected) {
-      Navigator.pushReplacementNamed(context, Home.route);
+      return true;
     } else {
       ErrorDialog.show(
         context,
@@ -28,12 +35,46 @@ class Loading extends StatelessWidget {
         message:
             'Make sure your device is connected to the network and then try again.',
       );
+      return false;
     }
   }
 
+  /// Checks whether the app has external storage read/write permission or not.
+  ///
+  /// If permission is granted, returns true.
+  /// Otherwise requests for permission. If permission denied again,
+  /// it shows an error dialog returns false.
+  Future<bool> _checkStoragePermission(BuildContext context) async {
+    if (await Permission.storage.isGranted) {
+      return true;
+    } else {
+      var status = await Permission.storage.request();
+      if (status == PermissionStatus.granted) {
+        return true;
+      } else {
+        ErrorDialog.show(
+          context,
+          title: 'Storage Permission Denied',
+          message:
+              'This app needs storage access permission to work. Go to your device\'s settings and grant permission',
+        );
+
+        return false;
+      }
+    }
+  }
+
+  /// Calls [_checkNetworkConnection] and [_checkStoragePermission].
+  /// If both returned true,
+  /// then [Home] page will be pushed to navigator
   void _initialStuffs(BuildContext context) async {
     await SettingsService.initialize();
-    await _checkNetworkConnection(context);
+    bool isConnected = await _checkNetworkConnection(context);
+    bool hasPermission = await _checkStoragePermission(context);
+
+    if (isConnected && hasPermission) {
+      Navigator.pushReplacementNamed(context, Home.route);
+    }
   }
 
   @override
