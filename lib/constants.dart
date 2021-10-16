911b317793cbf39ed2f524e61b466eff704c2820
mark <mr.karimi1377@gmail.com>
2021-10-16 21:29:34 +0330

    inactive color added

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xffc13584);
 const Color kPrimaryShadowColor = Color(0x45c13584);
+const Color kPrimaryInactiveColor = Color(0x77c13584);
